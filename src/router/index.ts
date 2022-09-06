2f84a9e1b83be774336b687ed519ebf55045ad91
Hasegawa Sean <hasegawasean@Hasegawas-MacBook-Pro.local>
2022-09-06 15:59:40 +0900

    fixed spacing

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,19 +3,19 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 const routes = [
-    {
-        path: '/',
-        name: '',
-        component: () => import(/* webpackChunkName: "github_info" */ '@/features/github_info/views/IndexPage.vue'),
-        meta: {
-            withHeader: true,
-            requiresAuth: true
-        }
+  {
+    path: '/',
+    name: '',
+    component: () => import(/* webpackChunkName: "github_info" */ '@/features/github_info/views/IndexPage.vue'),
+    meta: {
+      withHeader: true,
+      requiresAuth: true
     }
+  }
 ];
 
 const router = new VueRouter({
-    routes
+  routes
 });
 
 export default router;
--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -2,31 +2,31 @@ import { createDecorator } from "vue-class-component";
 import _ from "lodash";
 
 export const Debounce = (waitMs: number) =>
-    createDecorator((options, key) => {
-        if (options.methods && options.methods[key]) {
-            const originalMethod = options.methods[key];
-            const debounceMethod = _.debounce(originalMethod, waitMs, {
-                leading: false,
-                trailing: true,
-            });
+  createDecorator((options, key) => {
+    if (options.methods && options.methods[key]) {
+      const originalMethod = options.methods[key];
+      const debounceMethod = _.debounce(originalMethod, waitMs, {
+        leading: false,
+        trailing: true,
+      });
 
-            options.methods[key] = async function (...args) {
-                await debounceMethod.apply(this, args);
-            };
-        }
-    });
+      options.methods[key] = async function (...args) {
+        await debounceMethod.apply(this, args);
+      };
+    }
+  });
 
 export const Throttle = (waitMs: number) =>
-    createDecorator((options, key) => {
-        if (options.methods && options.methods[key]) {
-            const originalMethod = options.methods[key];
-            const throttleMethod = _.throttle(originalMethod, waitMs, {
-                leading: true,
-                trailing: false,
-            });
+  createDecorator((options, key) => {
+    if (options.methods && options.methods[key]) {
+      const originalMethod = options.methods[key];
+      const throttleMethod = _.throttle(originalMethod, waitMs, {
+        leading: true,
+        trailing: false,
+      });
 
-            options.methods[key] = async function (...args) {
-                await throttleMethod.apply(this, args);
-            };
-        }
-    });
+      options.methods[key] = async function (...args) {
+        await throttleMethod.apply(this, args);
+      };
+    }
+  });
